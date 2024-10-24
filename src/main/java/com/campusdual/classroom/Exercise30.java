cbe750ba5a8ddb927848a82c7fce112c2dedf9c4
EmmaIP <emmapineiro@gmail.com>
2024-10-24 15:41:35 +0200

    ejercicio 30 finalizado

--- src/main/java/com/campusdual/classroom/Exercise30.java
+++ src/main/java/com/campusdual/classroom/Exercise30.java
@@ -2,11 +2,28 @@ package com.campusdual.classroom;
 
 public class Exercise30 {
 
-    public static int divisionWithCustomException(int dividend, int divisor){
-
+    public static int divisionWithCustomException(int dividend, int divisor) throws DivisionByZeroException {
+        if (divisor == 0) {
+            throw new DivisionByZeroException("No se puede dividir entre cero");
+        }
+        return dividend / divisor;
     }
 
     public static void main(String[] args) {
+        int dividend = 9;
+        int divisor= 0;
+
+        try {
+            int result = divisionWithCustomException(dividend, divisor);
+            System.out.println(result);
+
+        } catch (DivisionByZeroException e) {
+            System.out.println(e.getMessage());
+
+        } finally {
+            System.out.println("Se acaba esto");
+        }
+
 
     }
 }
